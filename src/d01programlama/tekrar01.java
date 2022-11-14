56f9b9bdbff9f225eebdb2dffa366ec627862f1a
MrBilek <118183604+MrBilek@users.noreply.github.com>
2022-11-14 16:24:08 +0300

    mymessage

---
+++ src/d01programlama/tekrar01.java
@@ -0,0 +1,10 @@
+package d01programlama;
+
+public class tekrar01 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");//print ln yazdırdıktan sonra alt satıra geçer
+        System.out.println("bugün kodlamada ilk günümüz");
+        System.out.println("buda 3.satır olsun");}
+
+
+}
